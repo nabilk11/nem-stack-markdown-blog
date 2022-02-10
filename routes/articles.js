c759127618d31d0d6067c42db20e6477c0c64049
nabilk11 <khan.nabiln11@gmail.com>
2022-02-10 02:36:56 -0500

    setup initial route for /articles path

--- routes/articles.js
+++ routes/articles.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+///Routes relative to /articles path///
+// GET Route
+router.get('/', (req, res) => {
+    res.send("in articles directory")
+})
+
+
+module.exports = router;
 
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/articles", articleRouter)
 
 //Home Route
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index", {articles: articles})
 })
 //404 Route
 
