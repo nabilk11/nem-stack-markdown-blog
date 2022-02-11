165cf71042a5dd854c0885dcbf7dd272ea87e485
nabilk11 <khan.nabiln11@gmail.com>
2022-02-11 00:47:39 -0500

    created saveAndRedirect middleware function for create and update routes, added edit page, and added method override to put, edit button works. app now has full CRUD

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 This is a full markdown blog application, using the NEM stack (Node.js, Express, &amp; MongoDB). The frontend UI is being rendered with ejs.
 
 
-# Markdown
+## Markdown
 In the vein of this README, the application will accept blogs in the markdown format, using a dependency known as marked.
 
-# Dependencies
+## Dependencies
 I used a series of different modules for this blog application, including the following:
 - Express - The framework upon which our application is built and allows us to perform routing etc. 
 - Nodemon - Allows for constant updates and continuous running of the application. 
 - EJS - Used EJS format in order to render our views for the blog.
 - Mongoose - Connection to our MongoDB Atlas Database.
 - dotenv - For our .env file
-- Marked - Allows us to render the blogs in markdown to html.
-- Multer - Will use in the later version of the application in order to give individuals the ability to upload images alongside their blogs.
-- Slugify - To allow us to clean up our URLs 
 - Method-Override - In order to override our form method and create a delete route.
-- dompurify -
+- Slugify - Allows us to create a slug, set slug to slugify'd version of title and clean up our URLs for show page.
+- Marked - marked.parse allows us to render the blogs markdown text to html.
+- dompurify - used with jsdom to create a sanitized html version of our markdown text.
 - jsdom - in order to allow html rendering within Node
+- Multer - FUTURE: Will use in the later version of the application in order to give individuals the ability to upload images alongside their blogs.
 
--- routes/articles.js
+++ routes/articles.js
@@ -8,6 +8,13 @@ router.get('/new', (req, res) => {
     res.render("articles/new", {article: new Article})
 })
 
+// EDIT - GET Router for Editing Articls
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render("articles/edit", {article: article})
+})
+
+
 // SHOW - GET Route for Articles
 router.get('/:slug', async (req, res) => {
     const article = await Article.findOne({
@@ -16,29 +23,46 @@ router.get('/:slug', async (req, res) => {
     res.render('articles/show', {article: article})
 })
 
-// CREATE - POST Route for Create New Articles 
-router.post('/', async (req, res) => {
-    let article = new Article({
-      title: req.body.title,
-      description: req.body.description,
-      markdown: req.body.markdown,    
-    })
-    try     { 
-       article = await article.save()
-       res.redirect(`/articles/${article.slug}`)
-    } catch (err) {
-        console.log(`Error in Create: ${err}.`)
-        res.render('articles/new', {article: article})
+// CREATE - POST Route for Create New Articles (uses middlewate saveAndRedirect)
+router.post('/', async (req, res, next) => {
+   req.article = new Article()
+    next()
+}, saveAndRedirect('new'))
 
-    }
-    
-})
+
+// UPDATE - PUT Route for Updating Edited Articles
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+     next()
+ }, saveAndRedirect('edit'))
+
+
 
 // DELETE - DELETE Route for Articles
 router.delete('/:id', async (req, res) => {
 await Article.findByIdAndDelete(req.params.id) 
 res.redirect('/')
 })
+
+
+// CREATE/UPDATE Route Middlware
+function saveAndRedirect (path) {
+return async (req, res) => {
+    let article = req.article
+        article.title = req.body.title
+        article.description = req.body.description
+        article.markdown = req.body.markdown   
+      
+      try     { 
+         article = await article.save()
+         res.redirect(`/articles/${article.slug}`)
+      } catch (err) {
+          console.log(`Error in Create: ${err}.`)
+          res.render(`articles/${path}`, {article: article})
+  
+      }
+}
+}
 
 
 
